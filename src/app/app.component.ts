c95b2561f79a0a9623704ffd65edf3bab97ad4de
Texterly <88582978+Texterly@users.noreply.github.com>
2023-07-03 18:20:56 +0300

    refactoring text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +14,12 @@ export class AppComponent {
   title = 'SecondProject';
 
   toggle = true;
+
+  cards: Card[] = [
+    {title: 'Card 1', text: 'This is card number 1'},
+    {title: 'This is card 2', text: 'This is card number 2'},
+    {title: 'Last Card', text: 'This is card number 3'},
+  ]
 
   toggleCards() {
     this.toggle = !this.toggle;
